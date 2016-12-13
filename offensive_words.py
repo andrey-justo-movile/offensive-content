0ade664edf6ab2ac83da54037703a3ee2ccf271f
andrey-justo-movile <andrey.justo@movile.com>
2016-12-13 09:37:17 -0200

    match exactly words

--- offensive_words.py
+++ offensive_words.py
@@ -6,29 +6,32 @@ Created on Dec 7, 2016
 '''
 
 import codecs
-from nltk.stem.snowball import SnowballStemmer
+import time
+import re
 
 class OffensiveWords():
 
     def __init__(self, language):
-        self.stemmer = SnowballStemmer(language)
-        self.lang_bad_words = set(self.stemmer.stem(w) for w in [line.strip() for line in codecs.open("./" + language + ".txt", 'r', "utf-8")])
-        print(self.lang_bad_words)
+        self.lang_bad_words = set(w for w in [line.strip() for line in codecs.open("./" + language + ".txt", 'r', "utf-8")])
+        self.delimiter_regex = ' |\\.|\\?|\\!|,|;'
         
     def badwords(self, words):
-        
-        for word in words:
-
-            if self.stemmer.stem(word).lower() in self.lang_bad_words:
+        # we should split by any other characters
+        for word in re.split(self.delimiter_regex, words):
+            if word in self.lang_bad_words:
                 yield word
                               
 def main():            
-    bad_words = list(OffensiveWords("portuguese").badwords("adoro esse programa, vai se FoDer".split(" ")))
+    bad_words = list(OffensiveWords("portuguese").badwords("Mas afinal o que é um filho da puta? Não obstante a frequência com que o termo é empregue na classificação axiomática de reconhecidos sociopatas como banqueiros, especuladores financeiros e ministros, não existe ainda consenso bastante para alcançar uma definição acabada. Longe de pretender ser tratado sobre o tema, este artigo é um subsídio para uma definição mínima. Caso o leitor responda afirmativamente a, pelo menos, cinco questões, então é provável que seja um filho da puta."))
+                     
     for w in bad_words:
         print(w)
     
     if len(bad_words) > 0:
         print("OFFENSIVE PHRASE")
         
+start = time.time()
 main()
-    
+print()
+end = time.time()
+print(end - start)
